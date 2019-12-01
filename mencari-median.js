a708565978ac867a1d87cb8287adc137265423e1
Adam Rafiandri <rafiandria23@gmail.com>
2019-12-01 09:33:23 +0700

    Solved!

--- mencari-median.js
+++ mencari-median.js
@@ -7,3 +7,25 @@
 
 
 
+function cariMedian(arr) {
+    let result = [];
+    let arrLeftHalf = arr.slice(0, arr.length / 2);
+    let arrRightHalf = arr.slice(arr.length / 2);
+
+    if (arr.length % 2 === 0) {
+        let median = (arrLeftHalf[arrLeftHalf.length - 1] + arrRightHalf[0]) / 2;
+        result.push(median);
+    } else if (arr.length % 2 !== 0) {
+        let median = arr[arrLeftHalf.length];
+        result.push(median);
+    }
+
+    return result;
+}
+
+// TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
