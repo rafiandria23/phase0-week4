c889726d0c513514ca3182ab6c4def2802ffe1b6
Adam Rafiandri <rafiandria23@gmail.com>
2019-12-02 13:27:11 +0700

    Solved!

---
+++ mencari-modus.js
@@ -0,0 +1,45 @@
+/**
+ * HACKTIV8 Phase 0 - Logic Challenge - Mencari Modus
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+*/
+
+
+
+
+
+function cariModus(arr) {
+    let modeStack = [];
+    let result = [];
+
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                modeStack.push(arr[i]);
+            }
+        }
+    }
+
+    if (modeStack.length === 0) {
+        return -1;
+    }
+
+    result.push(modeStack[0]);
+
+    for (let k = 0; k < modeStack.length; k++) {
+        for (let l = k+1; l < modeStack.length; l++) {
+            if (modeStack[k] === modeStack[l]) {
+                return -1;
+            }
+        }
+    }
+
+    return result;
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
