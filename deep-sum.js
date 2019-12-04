ddc78681eaa873601a51be20eaa8f4fc75680d0f
Adam Rafiandri <rafiandria23@gmail.com>
2019-12-04 16:44:25 +0700

    Solved

---
+++ deep-sum.js
@@ -0,0 +1,76 @@
+/**
+ * HACKTIV8 Phase 0 - Logic Challenge - Deep Sum
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+*/
+
+
+
+
+
+function deepSum(arr) {
+    // Code disini
+    
+    if (arr.length === 0) {
+        return 'No number';
+    }
+    
+    const arrSum = function (arr) {
+        return arr.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+    }
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            for (let k = 0; k < arr[i][j].length; k++) {
+            result.push(arr[i][j][k]);
+            }
+        }
+    }
+
+    return arrSum(result);
+}
+
+//TEST CASE
+console.log(deepSum([
+    [
+        [4, 5, 6],
+        [9, 1, 2, 10],
+        [9, 4, 3]
+    ],
+    [
+        [4, 14, 31],
+        [9, 10, 18, 12, 20],
+        [1, 4, 90]
+    ],
+    [
+        [2, 5, 10],
+        [3, 4, 5],
+        [2, 4, 5, 10]
+    ]
+])); // 316
+
+console.log(deepSum([
+    [
+        [20, 10],
+        [15],
+        [1, 1]
+    ],
+    [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        [2],
+        [9, 11]
+    ],
+    [
+        [3, 5, 1],
+        [1, 5, 3],
+        [1]
+    ],
+    [
+        [2]
+    ]
+])); // 156
+
+console.log(deepSum([])); // No number
