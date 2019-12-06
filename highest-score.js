5ba546427e33b79986e579adb77b22cd9e4601ae
Adam Rafiandri <rafiandria23@gmail.com>
2019-12-06 10:55:46 +0700

    Revised

--- highest-score.js
+++ highest-score.js
@@ -9,71 +9,18 @@
 
 function highestScore(students) {
     // Code disini
-    if (students.length === 0) {
-        return {};
-    }
-
-    let foxes = [];
-    let wolves = [];
-    let tigers = [];
-
     let result = {};
 
     for (let i = 0; i < students.length; i++) {
-        if (students[i].class === 'foxes') {
-            foxes.push(students[i]);
-        } else if (students[i].class === 'wolves') {
-            wolves.push(students[i]);
-        } else if (students[i].class === 'tigers') {
-            tigers.push(students[i]);
-        }
-    }
-
-    if (foxes.length === 1) {
-        result.foxes = {
-            name: foxes[0].name,
-            score: foxes[0].score
-        };
-    }
-
-    for (let j = 0; j < foxes.length - 1; j++) {
-        if (foxes[j].score > foxes[j+1].score) {
-            result.foxes = {
-                name: foxes[j].name,
-                score: foxes[j].score
+        if (result[students[i].class] === undefined) {
+            result[students[i].class] = {
+                name: students[i].name,
+                score: students[i].score
             };
-        }
-    }
-
-    if (wolves.length === 1) {
-        result.wolves = {
-            name: wolves[0].name,
-            score: wolves[0].score
-        };
-    }
-
-    for (let k = 0; k < wolves.length - 1; k++) {
-        if (wolves[k].score > wolves[k+1].score) {
-            result.wolves = {
-                name: wolves[k].name,
-                score: wolves[k].score
-            };
-        }
-    }
-
-    if (tigers.length === 1) {
-        result.tigers = {
-            name: tigers[0].name,
-            score: tigers[0].score
-        };
-    }
-
-    for (let l = 0; l < tigers.length - 1; l++) {
-        if ((tigers[l].score > tigers[l + 1].score)) {
-            result.tigers = {
-                name: tigers[l].name,
-                score: tigers[l].score
-            };
+        } else {
+            if (result[students[i].class].score < students[i].score) {
+                result[students[i].class].score = students[i].score;
+            }
         }
     }
 
